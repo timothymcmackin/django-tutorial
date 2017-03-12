ce8bf1fb07b81c7b480e1fadbd361971fac376e6
timothymcmackin <timothymcmackin@yahoo.com>
2017-03-12 11:22:16 -0400

    hello world response in views

--- simple_blog/blogapp/views.py
+++ simple_blog/blogapp/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# return simple test string
+def index(request):
+    return HttpResponse("Test string!")
